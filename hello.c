c31d6c7c5695308672e67d9eba81b73a880eb92f
joshuarothfuss <joshua@rothfuss.us>
2022-08-25 19:39:28 +0000

    added Hello World code

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * author: Joshua ROthfuss
+ * Date: 2022/08/23
+ * Hack 1.0
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+
+    printf("Hello World! I'm Joshua Rothfuss!\n");
+
+    return 0;
+}
